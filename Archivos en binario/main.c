1d3f581980f0b03b246c33cda1aea37a73c22b16
alumno <alumno@utnfra.com.ar>
2019-10-28 10:28:33 -0300

    Ejercicio de Archivos en binario

---
+++ Archivos en binario/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int legajo;
+    char nombre[30];
+    float peso;
+
+}eDato;
+
+
+int main()
+{
+    FILE* pArchivo;
+    int x;
+    eDato miDato = {100,"Pepe",75};
+
+       pArchivo = fopen("miArchivo", "wb");
+       char miNombre[] = "Ramiro";
+
+    if(pArchivo != NULL)
+    {
+        fwrite(&miDato,sizeof(eDato),1,pArchivo);
+        fclose(pArchivo);
+    }
+
+
+    eDato midato2;
+
+    pArchivo = fopen("miArchivo","rb");
+
+    if(pArchivo != NULL)
+    {
+        fread(&midato2,sizeof(eDato),1,pArchivo);
+        printf("%s - %d - %f", midato2.nombre,midato2.legajo,midato2.peso);
+
+        fclose(pArchivo);
+    }
+
+    return 0;
+}
